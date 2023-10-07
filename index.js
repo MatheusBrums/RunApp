cc9d202ebc2f3058c9fd6da89fecc5809c1a3ff1
Matheus Brum <110873227+MatheusBrums@users.noreply.github.com>
2023-10-07 12:11:40 -0300

    restart program

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
+
+let stop = document.querySelector('#stopRun')
+//contador de tempo quando abre a runPage
 let time = document.getElementById('timeRun')
-//let distance = document.querySelector('#distanceRun')
-let speed = document.querySelector('#speedRun')
+let seconds = 0
+let minutes = 0
+let hours = 0
 
-seconds = 0
-minutes = 0
-hours = 0
-
-function twoNumbers(number){
-    if(number < 10){
+function twoNumbers(number) {
+    if (number < 10) {
         return `0${number}`
-    }else{
+    } else {
         return number
     }
 }
-function addTime(){
-   seconds++
-   if(seconds > 59){
+
+function addTime() {
+    seconds++
+    if (seconds > 59) {
         seconds = 0
         minutes++
-        if(minutes > 59){
+        if (minutes > 59) {
             minutes = 0
             hours++
         }
-   }
-   time.innerText = `${twoNumbers(hours)}:${twoNumbers(minutes)}:${twoNumbers(seconds)}`
-   
-   
+    }
+    time.innerText = `${twoNumbers(hours)}:${twoNumbers(minutes)}:${twoNumbers(seconds)}`
+
+
 }
-setInterval(addTime ,1000)
+setInterval(addTime, 1000)
 
 
 //Executando api para pegar a velocidade do player
-window.onload = function() {
-    function naviSuccess(position){
-        speed.innerHTML = position.coords.speed ? (position.coords.speed * 3.6).toFixed(1):0
-    }
-    function naviError(error){
-        alert('Error 404')
-    }
-    const options = {enableHighAccuracy: true,
-        timeout: 5000}
-    navigator.geolocation.watchPosition(naviSuccess, naviError, options)
+let speedPage = document.querySelector('#speedRun')
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+};
 
-/*
-//Pegano o tempo que o usuario esta correndo
-    let novaHora = new Date()
-    let hora = novaHora.getHours()
-    let minuto = novaHora.getMinutes()
-    let segundo = novaHora.getSeconds()
-    console.log(`${hora}:${minuto}:${segundo}`)
-    setInterval(function(){
-        let hora1 = novaHora.getHours() - hora
-        let minuto1 = novaHora.getMinutes() - minuto
-        let segundo1 = novaHora.getSeconds() - segundo
-        console.log(`${hora1}:${minuto1}:${segundo1}`)
-    },1000)
-*/
+function success(pos) {
+    const crd = pos.coords;
+    let speed = crd.speed
+    if(!speed){
+        speed = 0
+    }else{
+        speed = speed * 3.6
+    }
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+    console.log(`More or less ${speed} meters.`);
+    speedPage.innerText = `${speed}`
 }
- 
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.watchPosition(success, error, options);
